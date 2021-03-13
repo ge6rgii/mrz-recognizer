ae097efc31997440059c628523f5e28ed3e4d26d
Georgii <56611710+ge6rgii@users.noreply.github.com>
2021-03-14 00:14:59 +0300

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from recognizer import ImageProcessor, DataParser
-from pytesseract import image_to_string
-
-processor = ImageProcessor()
-mrz_area = processor.get_mrz_area("dataset/RUS1.jpg")
-mrz_string = image_to_string(mrz_area, lang="sabrina")
